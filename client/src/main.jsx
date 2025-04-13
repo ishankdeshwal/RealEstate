aa3898d8ebc7e29e95e971ad306db3d051dc0f29
Ishank <deshwalishank@gmail.com>
2025-04-14 02:03:37 +0530

    123

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import {Auth0Provider} from "@auth0/auth0-react"
 import { MantineProvider } from '@mantine/core'
 
 // Determine the redirect URI based on the environment
-// const redirectUri = window.location.origin;
+const redirectUri = window.location.origin;
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
@@ -14,8 +14,8 @@ createRoot(document.getElementById('root')).render(
   domain="dev-c36q3wiqqre4xdtr.us.auth0.com"
   clientId="2OVJ5t85LXV7QwIlPGxpGN7WdfNHeDSe"
   authorizationParams={{
-    redirect_uri: redirectUri,
-    audience: "https://realestate-frontend-7td2.onrender.com",
+    redirect_uri: "https://realestate-frontend-7td2.onrender.com",
+    audience: "https://realestate-backend-server.onrender.com",
     scope: "openid profile email offline_access"
   }}
   cacheLocation="localstorage"
