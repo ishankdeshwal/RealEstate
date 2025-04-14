624be55d1cdb218454f02ade0b4bd5d7325a75df
Ishank <deshwalishank@gmail.com>
2025-04-14 12:08:58 +0530

    hogyaa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,7 +23,7 @@ createRoot(document.getElementById('root')).render(
   advancedOptions={{
     defaultScope: "openid profile email offline_access"
   }}
-  skipRedirectCallback={window.location.pathname === '/callback'}
+  
 >
 
      <MantineProvider withGlobalStyles withNormalizeCSS>
