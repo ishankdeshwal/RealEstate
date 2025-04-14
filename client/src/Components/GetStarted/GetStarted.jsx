e5b6d7c35d519b714b19aeb8fd442bf10e5a97f1
Ishank <deshwalishank@gmail.com>
2025-04-14 13:38:41 +0530

    get started

--- client/src/Components/GetStarted/GetStarted.jsx
+++ client/src/Components/GetStarted/GetStarted.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserDetailContext from '../../Context/UserDetailsContext'
 
 function GetStarted() {
+  const navigate = useNavigate();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { userDetails } = useContext(UserDetailContext);
+
+  const handleGetStarted = () => {
+    if (!isAuthenticated) {
+      // If user is not logged in, redirect to login
+      loginWithRedirect();
+    } else {
+      // If user is logged in, navigate to properties page
+      navigate('/properties');
+    }
+  };
+
   return (
     <div className='pt-10'>
-        <div className='paddings innerWidth  '>
-                <div className=' g-inner flex flex-col justify-center items-center bg-[#4161df] gap-[1.5rem] p-6 rounded-xl border-[6px] border-[#5d77d6] text-center '>
+        <div className='paddings innerWidth'>
+                <div className='g-inner flex flex-col justify-center items-center bg-[#4161df] gap-[1.5rem] p-6 rounded-xl border-[6px] border-[#5d77d6] text-center'>
                     <span className='text-[1.5rem] font-bold text-white'>Get Started With Homyz</span>
                     <span className='text-[0.8rem] text-[rgb(255,255,255,0.587)]'>
                     Subscribe and find super attractive price quotes from us.
                     <br />
                     Find your residence soon
                     </span>
-                    <button className='button  text-xs w-30 h-20'>
-                      <a href="mailo:deshwalishank@gmail.com">
+                    <button 
+                      className='button text-xs w-30 h-20'
+                      onClick={handleGetStarted}
+                    >
                       Get Started
-                      </a>
                     </button>
                 </div>
         </div>
