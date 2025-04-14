ee69efc58457c4fe2a8f084d7746a4168055e989
Ishank <deshwalishank@gmail.com>
2025-04-14 13:44:32 +0530

    footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Footer() {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div className=' pt-15'>
-        <div className="paddings innerWidth flexCenter f-container border-t-[0.5px] border-[0,0,0,0.148] ">
+    <div className='pt-15'>
+        <div className="paddings innerWidth flexCenter f-container border-t-[0.5px] border-[0,0,0,0.148]">
             {/* left */}
             <div className='flexColStart gap-3'>
                 <img src="./logo2.png" width={120} alt="" />
@@ -15,12 +22,31 @@ function Footer() {
                 <span className='primaryText'>Information</span>
                 <span className='secondaryText'>145 newYork,Fl 2571, USA</span>
                 <div className='flex mt-[1.5rem] font-[500] gap-3'>
-                    <span>Property</span>
-                    <span>Services</span>
-                    <span>Product</span>
-                    <span>About Us</span>
+                    <span 
+                      className="cursor-pointer hover:text-blue-500 transition-colors"
+                      onClick={() => handleNavigation('/properties')}
+                    >
+                      Property
+                    </span>
+                    <span 
+                      className="cursor-pointer hover:text-blue-500 transition-colors"
+                      onClick={() => handleNavigation('/services')}
+                    >
+                      Services
+                    </span>
+                    <span 
+                      className="cursor-pointer hover:text-blue-500 transition-colors"
+                      onClick={() => handleNavigation('/about')}
+                    >
+                      About Us
+                    </span>
+                    <span 
+                      className="cursor-pointer hover:text-blue-500 transition-colors"
+                      onClick={() => handleNavigation('/contact')}
+                    >
+                      Contact Us
+                    </span>
                 </div>
-
             </div>
         </div>
     </div>
